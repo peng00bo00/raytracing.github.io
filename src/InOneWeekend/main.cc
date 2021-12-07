9ae13421078fc39e940a4f1deed51db307ac092f
peng00bo00 <pengbo_tongji@126.com>
2021-12-07 21:05:48 +0400

    finish Chap 9

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -15,6 +15,7 @@
 #include "hittable_list.h"
 #include "sphere.h"
 #include "camera.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -41,8 +42,15 @@ color ray_color(const ray& r, const hittable& world, int depth) {
 
     if (world.hit(r, 1e-3, infinity, rec)) {
         // point3 target = rec.p + rec.normal + random_unit_vector();
-        point3 target = rec.p + random_in_hemisphere(rec.normal);
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+
+        // point3 target = rec.p + random_in_hemisphere(rec.normal);
+        // return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+
+        ray scattered;
+        color attenuation;
+        if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
+            return attenuation * ray_color(scattered, world, depth-1);
+        return color(0,0,0);
     }
 
     // background
@@ -62,8 +70,18 @@ int main() {
 
     // World
     hittable_list world;
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+    // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Camera
     // auto viewport_height = 2.0;
