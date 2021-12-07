2fcfb4c115669bfcd1fab012cc744a1f84241e12
peng00bo00 <pengbo_tongji@126.com>
2021-12-07 20:12:33 +0400

    finish Chap 6

--- src/InOneWeekend/hittable_list.h
+++ src/InOneWeekend/hittable_list.h
@@ -19,9 +19,9 @@
 #include <vector>
 
 
-class hittable_list : public hittable  {
+class hittable_list : public hittable {
     public:
-        hittable_list() {}
+        hittable_list() {};
         hittable_list(shared_ptr<hittable> object) { add(object); }
 
         void clear() { objects.clear(); }
@@ -29,18 +29,18 @@ class hittable_list : public hittable  {
 
         virtual bool hit(
             const ray& r, double t_min, double t_max, hit_record& rec) const override;
-
+    
     public:
         std::vector<shared_ptr<hittable>> objects;
 };
 
-
 bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
     hit_record temp_rec;
-    auto hit_anything = false;
+    bool hit_anything = false;
     auto closest_so_far = t_max;
 
-    for (const auto& object : objects) {
+    for (const auto& object : objects)
+    {
         if (object->hit(r, t_min, closest_so_far, temp_rec)) {
             hit_anything = true;
             closest_so_far = temp_rec.t;
@@ -50,6 +50,5 @@ bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& re
 
     return hit_anything;
 }
-
 
 #endif
--- src/InOneWeekend/sphere.h
+++ src/InOneWeekend/sphere.h
@@ -14,11 +14,14 @@
 #include "rtweekend.h"
 
 #include "hittable.h"
+#include "vec3.h"
 
 
 class sphere : public hittable {
     public:
         sphere() {}
+
+        sphere(point3 cen, double r) : center(cen), radius(r), mat_ptr(nullptr) {};
 
         sphere(point3 cen, double r, shared_ptr<material> m)
             : center(cen), radius(r), mat_ptr(m) {};
